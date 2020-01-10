4f4441a05a6c844943e4d27ba5da4a6eed1f6cb0
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2020-01-10 17:28:14 +0530

    final layout

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-    <v-app id="overlay">
+    <v-app id="inherit">
       <v-content>
-        <span class="bg"></span>
+        <!-- <span class="bg"></span> -->
         <v-container fluid fill-height>
           <v-layout align-center justify-center>
             <v-flex xs12 sm8 md4 lg3 class="lg-11">
               <v-card
-                style=" left: 8% ; top: 15px; z-index: 2; position: relative;"
-                width="25%"
-                height="50"
+                style=" left: 8% ; top: 25px; z-index: 2; position: relative; "
+                width="30%"
                 left="25%"
-                class="mb-n2"
+                color="cyan"
+                dark
+                class="mx-0"
               >
-                <v-card-text>LOGIN</v-card-text>
+                <v-card-text class="px-0 py-3 text-center font-size: 43px">LOGIN</v-card-text>
               </v-card>
-              <v-card relative class="mb-n2" width="100%">
+              <v-card relative class width="100%"></v-card>
+              <v-card c :loading="state" :disabled="state">
                 <v-img class="white--text align-end" height="250px" src="Flat-Mountains.svg" />
-              </v-card>
-              <v-card :loading="state" :disabled="state">
                 <v-card-text class="pb-0 pt-7 px-11">
                   <v-form ref="form" lazy-validation>
                     <v-text-field
@@ -44,24 +44,25 @@
                       v-on:keyup.enter="validate"
                     ></v-text-field>
                   </v-form>
-                  <v-flex>
-                    <v-layout justify-center align-center>
-                      <v-card-actions width="20%" class="pt-0 pb-5 px-0" min-height="10px">
-                        <v-btn color="cyan" dark @click="validate">Login</v-btn>
-                        <v-spacer></v-spacer>
-                      </v-card-actions>
-                    </v-layout>
-                  </v-flex>
-                  <!-- <v-checkbox
-                    width="100%"
-                    v-model="checkbox"
-                    value="1"
-                    label="Remember me"
-                    :rules="[v => !!v || 'check here to remeber']"
-                    data-vv-name="checkbox"
-                    type="checkbox"
-                    required
-                  ></v-checkbox>-->
+                  <v-layout justify-center align-center>
+                    <v-checkbox
+                      class="mt-0"
+                      width="100%"
+                      v-model="checkbox"
+                      value="1"
+                      label="Remember me"
+                      :rules="[v => !!v || 'check here to remeber']"
+                      data-vv-name="checkbox"
+                      type="checkbox"
+                      required
+                    ></v-checkbox>
+                  </v-layout>
+                  <v-layout justify-center align-center>
+                    <v-card-actions width="20%" class="pt-0 pb-5 px-0" min-height="10px">
+                      <v-btn color="cyan" dark @click="validate">Login</v-btn>
+                      <v-spacer></v-spacer>
+                    </v-card-actions>
+                  </v-layout>
                 </v-card-text>
               </v-card>
             </v-flex>
